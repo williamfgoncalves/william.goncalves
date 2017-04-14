5776a4337295641a0be98c4809c1667aec7c75a2
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-14 01:37:42 -0300

    fix(Saint): Criado novos metodos para genero e vida

--- CavaleirosDoZodiacoModulo1/Saint.java
+++ CavaleirosDoZodiacoModulo1/Saint.java
@@ -1,13 +1,19 @@
 public class Saint{
     private String nome;
-    private Armadura armadura;
     private boolean armaduraVestida;
-    private Genero genero;
+    private double vida = 100.0;
+    private Armadura armadura;
+    private Genero genero = Genero.NAO_INFORMADO;
+    private StatusDeVida statusDeVida = StatusDeVida.VIVO;
     
     //construtor da classe
     public Saint(String nome, Armadura armadura){
         this.nome = nome; //agora nome recebe a string n
         this.armadura = armadura;
+    }
+    
+    public int getValorArmadura(){
+        return this.armadura.getCategoria();
     }
     
     public void vestirArmadura(){
@@ -16,5 +22,25 @@ public class Saint{
     
     public boolean getArmaduraVestida(){
         return this.armaduraVestida;
+    }
+    
+    public Genero getGenero(){
+        return this.genero;
+    }
+    
+    public void setGenero(Genero genero){
+        this.genero = genero;
+    }
+    
+    public StatusDeVida getStatusDeVida(){
+        return this.statusDeVida;
+    }
+    
+    public void perderVida(){
+       this.vida -= 10.0;       
+    }
+    
+    public double getVida(){
+        return this.vida;
     }
 }
