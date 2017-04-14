efee65e937bd0d63d35397f9423fbc1f2523e657
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-14 01:38:34 -0300

    feat(Genero): Criado metodo para buscar valor da categoria

--- CavaleirosDoZodiacoModulo1/Genero.java
+++ CavaleirosDoZodiacoModulo1/Genero.java
@@ -1,10 +1,3 @@
-
-/**
- * Enumeration class Genero - write a description of the enum class here
- *
- * @author (your name here)
- * @version (version number or date here)
- */
 public enum Genero
 {
     FEMININO, MASCULINO, NAO_INFORMADO;
