a4f5722c2b35ac1fd11a450d030161deefc2c19c
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-15 20:49:34 -0300

    fix(BatalhaTest): Corrigindo os testes e adicionando outro teste

--- CavaleirosDoZodiacoModulo1/BatalhaTest.java
+++ CavaleirosDoZodiacoModulo1/BatalhaTest.java
@@ -6,22 +6,32 @@ import org.junit.Test;
 public class BatalhaTest
 {
     @Test
-    public void testarBatalhaCategoriaDiferente(){
+    public void testarBatalhaSaintUmMaiorQueSaintDois(){
         Saint goku = new Saint("Goku", new Armadura("Pegasus", Categoria.OURO));
         Saint vegeta= new Saint("Vegeta", new Armadura("Latão", Categoria.PRATA));
-        Batalha batalha = new Batalha();
-        batalha.iniciarBatalha(goku, vegeta);
-        assertEquals(goku.getVida(), 100.0, 0.1);
+        Batalha batalha = new Batalha(goku, vegeta);
+        batalha.iniciar();
+        assertEquals(100.0, goku.getVida(), 0.1);
         assertEquals(90.0, vegeta.getVida(), 0.1);
     }
     
     @Test
-    public void testarBatalhaCategoriaIgual(){
+    public void testarBatalhaSaintUmIgualSaintDois(){
         Saint shiryu = new Saint("shiryu", new Armadura("Dragão", Categoria.OURO));
         Saint ikki = new Saint("ikki", new Armadura("Fenix", Categoria.OURO));
-        Batalha batalha = new Batalha();
-        batalha.iniciarBatalha(shiryu, ikki);
+        Batalha batalha = new Batalha(shiryu, ikki);
+        batalha.iniciar();
         assertEquals(100.0, shiryu.getVida(), 0.1);
         assertEquals(90.0, ikki.getVida(), 0.1);
+    }
+    
+    @Test
+    public void testarBatalhaSaintDoisMaiorQueSaintUm(){
+        Saint shiryu = new Saint("shiryu", new Armadura("Ornitorrinco", Categoria.PRATA));
+        Saint ikki = new Saint("ikki", new Armadura("Fenix", Categoria.OURO));
+        Batalha batalha = new Batalha(shiryu, ikki);
+        batalha.iniciar();
+        assertEquals(90.0, shiryu.getVida(), 0.1);
+        assertEquals(100.0, ikki.getVida(), 0.1);
     }
 }
