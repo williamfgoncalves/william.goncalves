225d2e5390b84e6df4515eb3a253ee58e25e5d2a
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-17 17:31:14 -0300

    refactor(Batalha): Usar a variavel dano dinamicamente para gerar dano ao Saint

--- CavaleirosDoZodiacoModulo1/Batalha.java
+++ CavaleirosDoZodiacoModulo1/Batalha.java
@@ -2,6 +2,7 @@ public class Batalha{
     
     private Saint s1;
     private Saint s2;
+    private double dano = 10;
     
     public Batalha(Saint s1, Saint s2){
         this.s1 = s1;
@@ -11,9 +12,9 @@ public class Batalha{
     public void iniciar(){
     
        if(s1.getValorArmadura() == s2.getValorArmadura() || s1.getValorArmadura() > s2.getValorArmadura()){
-            s2.perderVida(10.0);
+            s2.perderVida(dano);
         }else{
-            s1.perderVida(10.0);
+            s1.perderVida(dano);
         }
     }
 }
