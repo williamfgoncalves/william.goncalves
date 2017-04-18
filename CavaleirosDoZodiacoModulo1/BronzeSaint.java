529a39c84925a6f2c4e701cedc8c6dc55c4d757c
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-17 23:10:00 -0300

    Feat(Bronze, Silver e GoldSaint) Criação de classe filhas de Saint

---
+++ CavaleirosDoZodiacoModulo1/BronzeSaint.java
@@ -0,0 +1,7 @@
+public class BronzeSaint extends Saint{
+    
+    public BronzeSaint(String nome, Armadura armadura)throws Exception{
+        super(nome, armadura);
+        this.qtdSentidosDespertados = 5;
+    }
+}
---
+++ CavaleirosDoZodiacoModulo1/SilverSaint.java
@@ -0,0 +1,7 @@
+public class SilverSaint extends Saint{
+    
+    public SilverSaint(String nome, Armadura armadura)throws Exception{
+        super(nome, armadura);
+        this.qtdSentidosDespertados = 6;
+    }
+}
