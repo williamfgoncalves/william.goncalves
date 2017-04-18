5a845714accfea19d075eb5d21ebf9ffdf83a059
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-18 02:24:12 -0300

    Refactor(GoldSaint): Removendo encadeamento de if's, agora temos uma classe separada para gerenciar as constelacoes

--- CavaleirosDoZodiacoModulo1/GoldSaint.java
+++ CavaleirosDoZodiacoModulo1/GoldSaint.java
@@ -3,22 +3,5 @@ public class GoldSaint extends Saint{
     public GoldSaint(String nome, Armadura armadura)throws Exception{
         super(nome, armadura);
         this.qtdSentidosDespertados = 7;
-        
-        String constelacao = armadura.getConstelacao();
-        if ( !constelacao.equals("Áries") 
-        && !constelacao.equals("Touro")
-        && !constelacao.equals("Gêmeos")
-        && !constelacao.equals("Câncer")
-        && !constelacao.equals("Virgem")
-        && !constelacao.equals("Leão")
-        && !constelacao.equals("Libra")
-        && !constelacao.equals("Escorpião")
-        && !constelacao.equals("Sagitário")
-        && !constelacao.equals("Capricórnio")
-        && !constelacao.equals("Aquário")
-        && !constelacao.equals("Peixes")) {
-            // dar erro
-            throw new Exception("Esta Constelação é Invalida!!");
-        }
     }
 }
