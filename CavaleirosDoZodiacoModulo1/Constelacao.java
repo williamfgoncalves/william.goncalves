1e3f9b7e95b90bb96fe853eb3d63239b92c93e7f
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-18 00:21:52 -0300

    fix(Constelacao): Criação dos metodos para adicionar Golpes e retornar um array de golpes

--- CavaleirosDoZodiacoModulo1/Constelacao.java
+++ CavaleirosDoZodiacoModulo1/Constelacao.java
@@ -1,4 +1,15 @@
 public class Constelacao{
     private String nome;
     private Golpe[] golpes = new Golpe[3];
+    private int contador = 0;
+    
+    public void AdicionarGolpe(Golpe golpe){
+        if(contador < 3){
+            this.golpes[contador] = golpe;
+        }
+    }
+    
+    public Golpe[] getGolpes(){
+        return this.golpes;
+    }
 }
