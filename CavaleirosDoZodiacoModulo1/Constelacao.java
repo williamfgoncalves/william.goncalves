25e852859289586bffabd5a85f04927c38dfd4de
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-18 02:18:35 -0300

    Fix(Constelacao): Adicionando golpes da ultima para primeira posicao conforme enunciado e adicionando classe de teste

--- CavaleirosDoZodiacoModulo1/Constelacao.java
+++ CavaleirosDoZodiacoModulo1/Constelacao.java
@@ -1,11 +1,16 @@
 public class Constelacao{
     private String nome;
     private Golpe[] golpes = new Golpe[3];
-    private int contador = 0;
+    private int contador = 2;
     
-    public void AdicionarGolpe(Golpe golpe){
-        if(contador < 3){
+    public Constelacao(String nome){
+        this.nome = nome;
+    }
+    
+    public void adicionarGolpe(Golpe golpe){
+        if(contador >= 0){
             this.golpes[contador] = golpe;
+            contador--;
         }
     }
     
