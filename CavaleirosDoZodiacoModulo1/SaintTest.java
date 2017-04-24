980dacd582d6cb988c91ebdd408850482c321094
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-23 23:04:09 -0300

    Test(SaintTest): Adicionando alguns testes que estavam faltando

--- CavaleirosDoZodiacoModulo1/SaintTest.java
+++ CavaleirosDoZodiacoModulo1/SaintTest.java
@@ -38,13 +38,19 @@ public class SaintTest{
     }
 
     @Test
-    public void verificaStatusVida()throws Exception{
+    public void verificaSaintNasceVIVO()throws Exception{
         Saint groot = new BronzeSaint("Groot","Capricornio");
         assertEquals(Status.VIVO, groot.getStatus()); 
     }
 
     @Test
-    public void verificaPerdaVida()throws Exception{
+    public void verificarSaintNasceCom100deVida()throws Exception{
+        Saint Seya = new SilverSaint("Seya","DragonBorn");
+        assertEquals(100.0, Seya.getVida(), 0.1);
+    }
+
+    @Test
+    public void verificaPerdaVida16()throws Exception{
         Saint Seya = new SilverSaint("Seya","Virgem");
         Seya.perderVida(16.0);
         assertEquals(84.0, Seya.getVida(), 0.1);
@@ -89,6 +95,11 @@ public class SaintTest{
         assertEquals(7, Seya.getQtdSentidosDespertados());
     }
 
+    @Test(expected=Exception.class)
+    public void constelacaoInvalidaParaCategoriaOuro()throws Exception{
+        Saint dino = new GoldSaint("Dino", "Dinossauro");
+    }
+
     @Test
     public void verificaSaintPerdeVidaMorre()throws Exception{
         Constelacao sagitario = new Constelacao("Sagitário");
@@ -116,7 +127,82 @@ public class SaintTest{
     }
 
     @Test
-    public void saintProximoGolpeCorreto() throws Exception{
+    public void saintAprenderDoisGolpesAdicionaNaLista() throws Exception{
+        GoldSaint Seya = new GoldSaint("Seya","Sagitário");
+        Golpe hadukem = new Golpe("Hadukem", 20);
+        Golpe chuteDoDragao = new Golpe("Chute Do Dragao", 35);
+        Seya.aprenderGolpes(hadukem);
+        Seya.aprenderGolpes(chuteDoDragao);
+        assertEquals(hadukem, Seya.getArmadura().getConstelacao().getGolpes().get(0));
+        assertEquals(chuteDoDragao, Seya.getArmadura().getConstelacao().getGolpes().get(1));
+    }
+
+    @Test
+    public void saintAprenderTresGolpesAdicionaNaLista() throws Exception{
+        GoldSaint Seya = new GoldSaint("Seya","Sagitário");
+        Golpe hadukem = new Golpe("Hadukem", 20);
+        Golpe chuteDoDragao = new Golpe("Chute Do Dragao", 35);
+        Golpe socoDoDragao = new Golpe("Soco Do Dragao", 45);
+        Seya.aprenderGolpes(hadukem);
+        Seya.aprenderGolpes(chuteDoDragao);
+        Seya.aprenderGolpes(socoDoDragao); 
+        assertEquals(hadukem, Seya.getArmadura().getConstelacao().getGolpes().get(0));
+        assertEquals(chuteDoDragao, Seya.getArmadura().getConstelacao().getGolpes().get(1));
+        assertEquals(socoDoDragao, Seya.getArmadura().getConstelacao().getGolpes().get(2));
+    }
+
+    @Test
+    public void saintAprenderQuatroGolpesAdicionaNaLista() throws Exception{
+        GoldSaint Seya = new GoldSaint("Seya","Sagitário");
+        Golpe hadukem = new Golpe("Hadukem", 20);
+        Golpe chuteDoDragao = new Golpe("Chute Do Dragao", 35);
+        Golpe socoDoDragao = new Golpe("Soco Do Dragao", 45);
+        Golpe explosaoDeFogo = new Golpe("Explosao De Fogo", 20);
+        Seya.aprenderGolpes(hadukem);
+        Seya.aprenderGolpes(chuteDoDragao);
+        Seya.aprenderGolpes(socoDoDragao);
+        Seya.aprenderGolpes(explosaoDeFogo);
+        assertEquals(hadukem, Seya.getArmadura().getConstelacao().getGolpes().get(0));
+        assertEquals(chuteDoDragao, Seya.getArmadura().getConstelacao().getGolpes().get(1));
+        assertEquals(socoDoDragao, Seya.getArmadura().getConstelacao().getGolpes().get(2));
+        assertEquals(explosaoDeFogo, Seya.getArmadura().getConstelacao().getGolpes().get(3));
+    }
+
+    @Test
+    public void saintProximoGolpeCorretoComUmaRequisicoes() throws Exception{
+        GoldSaint Seya = new GoldSaint("Seya","Sagitário");
+        Golpe hadukem = new Golpe("Hadukem", 20);
+        Seya.aprenderGolpes(hadukem);
+        assertEquals(hadukem, Seya.getProximoGolpe());
+    }
+
+    @Test
+    public void saintProximoGolpeCorretoComDuasRequisicoes() throws Exception{
+        GoldSaint Seya = new GoldSaint("Seya","Sagitário");
+        Golpe hadukem = new Golpe("Hadukem", 20);
+        Golpe kamehameha = new Golpe("kamehameha", 80);
+        Seya.aprenderGolpes(hadukem);
+        Seya.aprenderGolpes(kamehameha);
+        assertEquals(hadukem, Seya.getProximoGolpe());
+        assertEquals(kamehameha, Seya.getProximoGolpe());
+    }
+
+    @Test
+    public void saintProximoGolpeCorretoComTresRequisicoes() throws Exception{
+        GoldSaint Seya = new GoldSaint("Seya","Sagitário");
+        Golpe hadukem = new Golpe("Hadukem", 20);
+        Golpe kamehameha = new Golpe("kamehameha", 80);
+        Golpe meteoroPegaso = new Golpe("Meteoro Pegaso", 90);
+        Seya.aprenderGolpes(hadukem);
+        Seya.aprenderGolpes(kamehameha);
+        Seya.aprenderGolpes(meteoroPegaso);
+        assertEquals(hadukem, Seya.getProximoGolpe());
+        assertEquals(kamehameha, Seya.getProximoGolpe());
+        assertEquals(meteoroPegaso, Seya.getProximoGolpe());
+    }
+
+    @Test
+    public void saintProximoGolpeCorretoComQuatroRequisicoes() throws Exception{
         GoldSaint Seya = new GoldSaint("Seya","Sagitário");
         Golpe hadukem = new Golpe("Hadukem", 20);
         Golpe kamehameha = new Golpe("kamehameha", 80);
@@ -129,24 +215,54 @@ public class SaintTest{
         assertEquals(meteoroPegaso, Seya.getProximoGolpe());
         assertEquals(hadukem, Seya.getProximoGolpe());
     }
+
+    @Test
+    public void getCSV()throws Exception{
+        Saint Goku = new GoldSaint("Goku","Áries");
+        String esperado = "Goku,100.0,Áries,OURO,VIVO,NAO_INFORMADO,false";
+        assertEquals(esperado, Goku.getCSV());
+    }
     
+    @Test
+    public void getCSVComGeneroTrocado()throws Exception{
+        Saint Goku = new GoldSaint("Goku","Áries");
+        Goku.setGenero(Genero.FEMININO);
+        String esperado = "Goku,100.0,Áries,OURO,VIVO,FEMININO,false";
+        assertEquals(esperado, Goku.getCSV());
+    }
+    
+    @Test
+    public void getCSVComPerdeVida()throws Exception{
+        Saint Goku = new GoldSaint("Goku","Áries");
+        Goku.perderVida(16.0);
+        String esperado = "Goku,84.0,Áries,OURO,VIVO,NAO_INFORMADO,false";
+        assertEquals(esperado, Goku.getCSV());
+    }
+    
+    @Test
+    public void getCSVComNomeNulo()throws Exception{
+        Saint Goku = new GoldSaint(null,"Áries");
+        String esperado = "null,100.0,Áries,OURO,VIVO,NAO_INFORMADO,false";
+        assertEquals(esperado, Goku.getCSV());
+    }
+
     @Test
     public void proximoMovimentoManeiraCircular()throws Exception{
         GoldSaint Aldeibaran = new GoldSaint("Aldeibaran", "Touro");
         GoldSaint Shiryu = new GoldSaint("Shiryu", "Libra");
         Golpe Hadukem = new Golpe("Hadukem", 10);
         Aldeibaran.aprenderGolpes(Hadukem);
-        
+
         Golpear lutchaLibre = new Golpear (Aldeibaran, Shiryu);
         Aldeibaran.adicionarMovimento(lutchaLibre);
         Aldeibaran.getProximoMovimento().executar();
         assertEquals(90.0,Shiryu.getVida(),0.1);
-        
+
         VestirArmadura colocaCasaco = new VestirArmadura(Aldeibaran);
         Aldeibaran.adicionarMovimento(colocaCasaco);
         Aldeibaran.getProximoMovimento().executar();
         assertEquals(true,Aldeibaran.getArmaduraVestida());
-        
+
         Aldeibaran.getProximoMovimento().executar();
         assertEquals(50.0,Shiryu.getVida(),0.1);
     }
