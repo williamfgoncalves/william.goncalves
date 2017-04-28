cd762beee8077af9ecf2082b7ee93d7a9af7830f
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-28 01:58:10 -0300

    Refactor(SorteDoDia): Remoção de metodo desnecessario

--- CavaleirosDoZodiacoModulo1/SorteDoDia.java
+++ CavaleirosDoZodiacoModulo1/SorteDoDia.java
@@ -1,18 +1,13 @@
 public class SorteDoDia{
-    
+
     private Sorteador sorteador;
-    
+
     public SorteDoDia(Sorteador sorteador){
         this.sorteador = sorteador;
     }
-    
+
     public boolean estouComSorte(){
         int resultado = sorteador.sortear();
         return resultado % 2 == 0;
-    }
-    
-    public boolean consigoDeferirAtaqueDuplo(){
-        int resultado = sorteador.sortear();
-        return resultado % 3 == 0;
     }
 }
