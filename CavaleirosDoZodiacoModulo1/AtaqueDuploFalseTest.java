b5aaebe9c40ec38cdaa3739314e3a4bef78e6b12
William Gonçalves <william.goncalves@cwi.com.br>
2017-04-29 23:16:45 -0300

    Feat(AtaqueDuploFalso): Criação de uma classe de ataque duplo falsa
    Essa classe simula o ataque duplo  com um dado com valor estatico e os testes sequem o mesmo padrão

--- CavaleirosDoZodiacoModulo1/AtaqueDuploFalseTest.java
+++ CavaleirosDoZodiacoModulo1/AtaqueDuploFalseTest.java
@@ -3,11 +3,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AtaqueDuploTest{
-   
+public class AtaqueDuploFalseTest{
     @Test
     public void verificaSeRealizaAtaqueDuploComArmadura() throws Exception{
-        
+
         Saint goku = new GoldSaint("Goku", "Câncer");
         goku.aprenderGolpes( new Golpe("Hadukem", 10));
 
@@ -15,36 +14,26 @@ public class AtaqueDuploTest{
         vegeta.aprenderGolpes(new Golpe("kamehameha", 30));
         goku.adicionarMovimento(new VestirArmadura(goku));
         goku.getProximoMovimento().executar();
-        goku.adicionarMovimento(new AtaqueDuplo(goku, vegeta));
+        goku.adicionarMovimento(new AtaqueDuploFalso(goku, vegeta));
         goku.getProximoMovimento().executar();
-        
-        if(vegeta.getVida() == 20){
-            assertEquals(100.0, goku.getVida(), 0.1);
-            assertEquals(20.0, vegeta.getVida(), 0.1);
-        }else{
-            assertEquals(95.0, goku.getVida(), 0.1);
-            assertEquals(60.0, vegeta.getVida(), 0.1);
-        }
+
+        assertEquals(100.0, goku.getVida(), 0.1);
+        assertEquals(20.0, vegeta.getVida(), 0.1);
     }
-    
+
     @Test
     public void verificaSeRealizaAtaqueDuploSemArmadura() throws Exception{
-        
+
         Saint goku = new GoldSaint("Goku", "Câncer");
         goku.aprenderGolpes( new Golpe("Hadukem", 10));
 
         Saint vegeta= new SilverSaint("Vegeta", "Leão");
         vegeta.aprenderGolpes(new Golpe("kamehameha", 30));
 
-        goku.adicionarMovimento(new AtaqueDuplo(goku, vegeta));
+        goku.adicionarMovimento(new AtaqueDuploFalso(goku, vegeta));
         goku.getProximoMovimento().executar();
-        
-        if(goku.getVida() == 100){
-            assertEquals(100.0, goku.getVida(), 0.1);
-            assertEquals(80.0, vegeta.getVida(), 0.1);
-        }else{
-            assertEquals(95.0, goku.getVida(), 0.1);
-            assertEquals(90.0, vegeta.getVida(), 0.1);
-        }
+
+        assertEquals(100.0, goku.getVida(), 0.1);
+        assertEquals(80.0, vegeta.getVida(), 0.1);
     }
 }
---
+++ CavaleirosDoZodiacoModulo1/AtaqueDuploFalso.java
@@ -0,0 +1,34 @@
+public class AtaqueDuploFalso implements Movimento{
+
+    private Saint golpeador,golpeado;
+    DadoFalso dadoFalso = new DadoFalso(3);
+
+    public AtaqueDuploFalso(Saint golpeador, Saint golpeado){
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+
+    public void executar()throws Exception{
+
+        boolean consigoDeferirAtaqueDuplo = dadoFalso.sortear() < 4;
+        int fatorDano = 0;
+        double danoAtaqueDuploFalhou = (golpeador.getVida()*5)/100;
+
+        if(golpeador.getArmaduraVestida()){            
+            fatorDano = consigoDeferirAtaqueDuplo ?
+                (golpeador.getProximoGolpe().getFatorDano()*(1+golpeador.getValorArmadura()))*2
+            : golpeador.getProximoGolpe().getFatorDano()*(1+golpeador.getValorArmadura());
+        }
+        else{
+            fatorDano = consigoDeferirAtaqueDuplo ?
+                golpeador.getProximoGolpe().getFatorDano()*2
+            : golpeador.getProximoGolpe().getFatorDano();
+        }
+
+        golpeado.perderVida(fatorDano);
+
+        if(!consigoDeferirAtaqueDuplo){
+            golpeador.perderVida(danoAtaqueDuploFalhou);
+        }
+    }
+}
