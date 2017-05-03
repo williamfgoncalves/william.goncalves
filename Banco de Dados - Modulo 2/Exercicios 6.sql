428954555642afe90bc945767bca747385759250
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-03 11:47:57 -0300

    Feat(SQL - Exercicio 6): Realização da lista de exercicio 6
    Faltou o ultimo exercicio

---
+++ Banco de Dados - Modulo 2/Exercicios 6.sql
@@ -0,0 +1,41 @@
+------------------------------------------------------------------------------------------------
+
+SELECT TOP(1) PrimeiroNome, COUNT(*) AS Contagem
+	FROM (SELECT SUBSTRING(Cliente.Nome, 1, CHARINDEX(' ', Cliente.Nome)) AS PrimeiroNome
+	FROM Cliente) AS Resultado
+	GROUP BY Resultado.PrimeiroNome
+	ORDER BY Contagem DESC; 
+
+	-----------------------------------------------------------------------------------
+
+SELECT count(P.IDPedido) AS QuantidadeTotalDosPedidos, SUM(p.ValorPedido) AS ValorTotalDosPedidos
+	FROM Pedido p
+	WHERE p.IDPedido IN(
+	SELECT p.IDPedido FROM Pedido WHERE MONTH(P.DataPedido) = 04 AND YEAR(P.DataPedido) = 2017);
+
+	--------------------------------------------------------------------------------------------
+SELECT TOP 1 Cid.UF, COUNT(Cid.UF) AS numeroClientes
+	FROM Cliente Cli INNER JOIN Cidade Cid
+	ON Cid.IDCidade = Cli.IDCidade
+	GROUP BY Cid.UF
+	ORDER BY COUNT (cid.UF) desc
+
+SELECT TOP 1 Cid.UF, COUNT(Cid.UF) AS numeroClientes
+	FROM Cliente Cli INNER JOIN Cidade Cid
+	ON Cid.IDCidade = Cli.IDCidade 
+	GROUP BY Cid.UF
+	ORDER BY COUNT (cid.UF) ASC
+
+----------------------------------------------------------------------------------------------------
+
+Select * from Produto
+Insert into Produto (Nome, PrecoCusto, PrecoVenda, Situacao) VALUES ('Galocha Maragato', 35.67, 77.95, 'A')
+
+---------------------------------------------------------------------------------------------------------
+
+SELECT *
+FROM Produto p
+WHERE NOT EXISTS
+    (SELECT * 
+     FROM PedidoItem pdIt
+	 WHERE pdIt.IDProduto = p.IDProduto)  
