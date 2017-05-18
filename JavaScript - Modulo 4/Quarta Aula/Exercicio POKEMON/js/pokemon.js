1c500a71d1321ead40436c677107246f34acd726
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-18 10:56:42 -0300

    Refactor(JavaScript): Refatorando e melhorando o exercicio de JSON com pokemons

--- JavaScript - Modulo 4/Quarta Aula/Exercicio POKEMON/js/pokemon.js
+++ JavaScript - Modulo 4/Quarta Aula/Exercicio POKEMON/js/pokemon.js
@@ -1,58 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let btnPesquisar = document.getElementById('btnPesquisar');
-  btnPesquisar.onclick = function() {
-    var campo = document.getElementById('numeroPokemon').value;
-    var url = "http://pokeapi.co/api/v2/pokemon/" + campo + "/";
-    fetch(url)
-            .then(response => response.json())
-            .then(json => {
-            addImagem(json)
-            addNomePoke(json)
-            addIdPoke(json)
-            addListaPoke(json)
-            status(json);
-            console.log(json);
-            
-         })
-    }
-});
-
-function addImagem(json){
-    let div = document.getElementById("imagemPokemon");
-    let img = document.createElement("img"); 
-    img.src = json.sprites.front_default;
-    div.append(img);
-}
-function addNomePoke(json){
-    let div = document.getElementById("tituloPokemon");
-    let nomePoke = json.name;
-    div.append(nomePoke);
-}
-function addIdPoke(json){
-    let div = document.getElementById("idPokemon");
-    let numeroPoke = json.id;
-    div.append(numeroPoke);
-}
-function addListaPoke(json){
-    let div = document.getElementById("listaPokemon");
-    let listaPoke = document.createElement("ul");
-        json.types.forEach(type => {
-        let item = document.createElement("li");
-        item.innerHTML = type.type.name;
-        listaPoke.appendChild(item);
-    });
-    div.append(listaPoke);
-}
-
-function status(json){
-    let div = document.getElementById("progress");
-    json.stats.forEach(stat => {
-        let progress = document.createElement("progress");
-        let statusPoke = document.createElement("h6");
-        statusPoke.innerText = stat.stat.name;
-        progress.max = 100;
-        progress.value = stat.base_stat;
-        div.append(statusPoke);
-        div.append(progress);
-    });
-}
+   let btnPesquisar = document.getElementById('btnPesquisar');
+   btnPesquisar.onclick = function() {
+     var campo = document.getElementById('numeroPokemon').value;
+     var url = "http://pokeapi.co/api/v2/pokemon/" + campo + "/";
+     fetch(url)
+             .then(response => response.json())
+             .then(json => {
+             addToHtml(json);
+             status(json);
+             console.log(json);
+             
+          })
+     }
+ });
+ 
+ function addToHtml(json){
+     let div = document.getElementById("elementosPokemon");
+     let img = document.createElement("img"); 
+     img.src = json.sprites.front_default;
+     let nomePoke = document.createElement("h3");
+     nomePoke.innerText = json.name;
+     let numeroPoke = document.createElement("h3");
+     numeroPoke.innerText = json.id;
+     let listaPoke = document.createElement("h3");
+         json.types.forEach(type => {
+         let item = document.createElement("h4");
+         item.innerHTML = type.type.name;
+         listaPoke.appendChild(item);
+     });
+     div.appendChild(nomePoke);
+     div.appendChild(numeroPoke);
+     div.appendChild(img);
+     div.appendChild(listaPoke);
+ }
+ 
+ function status(json){
+     let div = document.getElementById("elementosPokemon");
+     json.stats.forEach(stat => {
+         let progress = document.createElement("progress");
+         let statusPoke = document.createElement("h4");
+         statusPoke.innerText = stat.stat.name;
+         progress.max = 100;
+         progress.value = stat.base_stat;
+         div.appendChild(statusPoke);
+         div.appendChild(progress);
+     });
+ }
