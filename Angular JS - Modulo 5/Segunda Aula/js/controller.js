875ac1c1c076aa206b97f3a545f03adc9c96b8e1
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-18 17:19:06 -0300

    Feat(AngularJS): Exemplo de criação para filtro customizado

--- Angular JS - Modulo 5/Segunda Aula/js/controller.js
+++ Angular JS - Modulo 5/Segunda Aula/js/controller.js
@@ -14,3 +14,46 @@ controler.controller('controllerData', function($scope, $filter) {
                 $scope.dataPronta = $filter('date')(data, 'shortDate')
         }
 });
+
+controler.filter('mascada', function(){
+        return function(nome){
+                return nome.replace(/(nunes)/gi, '$ $1 $');
+        }
+});
+
+controler.controller('controllerInstrutores', function($scope) {
+        $scope.instrutores = [{
+                nome: 'Bernardo',
+                aula: [{
+                        numero: 1,
+                        nome: 'OO'
+                },
+                {
+                        numero: 4,
+                        nome: 'Javascript'
+                }
+                ]
+                },
+                {
+                        nome: 'Nunes',
+                        aula: [{
+                        numero: 2,
+                        nome: 'Banco de Dados I'
+                }]
+                },
+                {
+                        nome: 'Pedro (PHP)',
+                        aula: [{
+                        numero: 3,
+                        nome: 'HTML e CSS'
+                }]
+                },
+                {
+                        nome: 'Zanatta',
+                        aula: [{
+                        numero: 5,
+                        nome: 'AngularJS'
+                }]
+                }
+                ];
+});
