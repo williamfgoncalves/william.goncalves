60143183217d4d690708650a4a9d5a05c0bd08b6
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-20 17:17:43 -0300

    Fix(AngularJS): Adicionar instrutores, editar e deletar instrutores e aulas

--- Angular JS - Modulo 5/CRUD/js/controller.js
+++ Angular JS - Modulo 5/CRUD/js/controller.js
@@ -3,30 +3,64 @@ var controller = angular.module('aula3', []);
 
 controller.controller('controllerForm', function($scope){
     
+    $scope.editarAula = false;
+    $scope.mostrarAulas = false;
+    $scope.mostrarInstrutor = false;
+    $scope.editarInstrutor = false;
+
     let idAula = 0;
+    let idInstrutor = 0;
 
-    $scope.addInstrutor = function addInstrutor(instrutor){
-        if($scope.form1.$valid){
-            $scope.instrutores.push(angular.copy(instrutor));
-            $scope.instrutor = {};
-        }   
-    }
-
-    $scope.addAula = function addAula(aula){
+    $scope.addAula = function (aula){
         if($scope.formAula.$valid){
             aula.id = idAula++;
             $scope.aulas.push(angular.copy(aula));
             $scope.aula = {};
+            $scope.mostrarAulas = true;
+        }
+    }
+    
+    $scope.editAula = function (aula) {
+         $scope.editarAula = true;
+         $scope.aulaAtual = aula;
+    }
+
+    $scope.salvar = function(aulaAtual){
+        let idaula = aulaAtual.id;
+        $scope.aulas.splice(idaula,1,aulaAtual);
+        delete $scope.aulaAtual;
+        $scope.editarAula = false;
+    }
+
+    $scope.removeAula = function (aula){
+        let idAula = $scope.aulas.indexOf(aula);
+	    $scope.aulas.splice(idAula, 1);
+    }
+    
+    $scope.addInstrutor = function (instrutor){
+        if($scope.form1.$valid){
+            instrutor.id = idInstrutor++;
+            $scope.instrutores.push(angular.copy(instrutor));
+            delete $scope.instrutor;
+            $scope.mostrarInstrutor = true;
         }   
     }
 
-
-    $scope.editAula = function editAula(aula){
-        return nome
+    $scope.editInstrutor = function (instrutor) {
+         $scope.editarInstrutor = true;
+         $scope.instrutorAtual = instrutor;
     }
 
-    $scope.removeAula = function removeAula(aula){
-        $scope.aulas.pop(aula);
+    $scope.salvarInstrutor = function(instrutorAtual){
+        let idInstrutor = instrutorAtual.id;
+        $scope.instrutores.splice(idInstrutor,1,instrutorAtual);
+        delete $scope.instrutorAtual;
+        $scope.editarInstrutor = false;
+    }
+
+    $scope.removeInstrutor = function (instrutor){
+        let idInstrutor = $scope.instrutores.indexOf(instrutor);
+	    $scope.instrutores.splice(idInstrutor, 1);
     }
 
     $scope.instrutores = [{
