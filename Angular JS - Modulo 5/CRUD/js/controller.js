6beb5b6e738184332b281622f9515d40c0ab7bc7
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-21 20:31:18 -0300

    feat(Angular JS): Alterações visuais e adição do Angular Toastr para exibição de mensagem

--- Angular JS - Modulo 5/CRUD/js/controller.js
+++ Angular JS - Modulo 5/CRUD/js/controller.js
@@ -1,12 +1,13 @@
 //Criacao do modulo 
-var controller = angular.module('aula3', []);
+var controller = angular.module('aula3', ['ngAnimate', 'toastr']);
 
-controller.controller('controllerForm', function($scope){
+controller.controller('controllerForm', function($scope, toastr){
     
     $scope.editarAula = false;
     $scope.editarInstrutor = false;
     $scope.mostrarAulas;
     $scope.mostrarInstrutor;
+    
     
     function gerarId(arr){
         let id;
@@ -19,19 +20,30 @@ controller.controller('controllerForm', function($scope){
     }
 
     function compararNomes(arr, obj){
-        
     let compare = false;
-
     for(let i = 0; i < arr.length; i++){
         if(arr[i].nome.localeCompare(obj.nome) === 0){
             compare = true;
         }
     }    
-    
         if(compare === true){
                 return 0;
         }
+        else{
+            return 1;
+        }
+    }
 
+    function compararEmails(arr, obj){
+    let compareEmail = false;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].email.localeCompare(obj.email) === 0){
+            compareEmail = true;
+        }
+    }    
+        if(compareEmail === true){
+                return 0;
+        }
         else{
             return 1;
         }
@@ -52,18 +64,20 @@ controller.controller('controllerForm', function($scope){
         if($scope.formAula.$valid){
             let compararNome = compararNomes($scope.aulas, aula);
             if(compararNome === 0){
-                alert("Esta aula já existe");
+                toastr.error("Esta aula já existe");
                 return;
             }
             aula.id = gerarId($scope.aulas);
             $scope.aulas.push(angular.copy(aula));
             delete $scope.aula;
+            toastr.success('Aula adicionada com sucesso!');
             $scope.mostrarAulas = true;
         }
     }
     
     $scope.editAula = function (aula) {
          $scope.editarAula = true;
+         toastr.warning('Você está editando uma aula!');
          $scope.aulaAtual = aula;
     }
 
@@ -71,13 +85,20 @@ controller.controller('controllerForm', function($scope){
         let idaula = aulaAtual.id;
         $scope.aulas.splice(idaula,1,aulaAtual);
         delete $scope.aulaAtual;
+        toastr.success('Aula alterada com sucesso!');
+        $scope.editarAula = false;
+    }
+
+    $scope.cancelar = function(aulaAtual){
+        delete $scope.aulaAtual;
+        toastr.warning('Você cancelou a edição da aula!');
         $scope.editarAula = false;
     }
 
     $scope.removeAula = function (aula){
         let idAula = $scope.aulas.indexOf(aula);
         if(verificarAula(aula.id)){
-            alert("Aula esta sendo utilizada por um professor, impossivel excluir!");
+            toastr.error("Aula esta sendo utilizada por um professor, impossivel excluir!");
             return;
         }
 	    $scope.aulas.splice(idAula, 1);
@@ -85,36 +106,58 @@ controller.controller('controllerForm', function($scope){
     
     $scope.addInstrutor = function (instrutor){
         if($scope.form1.$valid){
+
             let compararNome = compararNomes($scope.instrutores, instrutor);
             if(compararNome === 0){
-                alert("Este professor já existe");
+                toastr.error("Este instrutor já existe!");
                 return;
             }
+
+            let compararEmail = compararEmails($scope.instrutores, instrutor);
+            if(compararEmail === 0){
+                toastr.error("Este email já está cadastrado!");
+                return;
+            }
+
+            if(instrutor.urlFoto === null || typeof instrutor.urlFoto === "undefined"){
+                instrutor.urlFoto = "https://pescadordesonhos.files.wordpress.com/2009/12/1-anonimo-famoso-oow.jpg";
+            }
+
             instrutor.id = gerarId($scope.instrutores);
             $scope.instrutores.push(angular.copy(instrutor));
             delete $scope.instrutor;
-            $scope.mostrarInstrutor = true;
+            toastr.success('Instrutor adicionado com sucesso!');
+            $scope.mostrarEdicao = false;
         }   
     }
 
     $scope.editInstrutor = function (instrutor) {
          $scope.editarInstrutor = true;
+         toastr.warning('Você está editando o instrutor!');
          $scope.instrutorAtual = instrutor;
+    }
+
+    $scope.cancelarInstrutor = function(instrutorAtual){
+        delete $scope.instrutorAtual;
+        toastr.warning('Você cancelou a edição da aula!');
+        $scope.editarInstrutor = false;
     }
 
     $scope.salvarInstrutor = function(instrutorAtual){
         let idInstrutor = instrutorAtual.id;
         $scope.instrutores.splice(idInstrutor,1,instrutorAtual);
         delete $scope.instrutorAtual;
+        toastr.success('Instrutor alterado com sucesso!');
         $scope.editarInstrutor = false;
     }
 
     $scope.removeInstrutor = function (instrutor){
         if(instrutor.dandoAula.localeCompare('SIM') === 0){
-            alert('Você não pode excluir esse professor, ele está dando aula!');
+            toastr.error('Você não pode excluir esse instrutor, ele está dando aula!');
             return;
         }
         let idInstrutor = $scope.instrutores.indexOf(instrutor);
+        toastr.error('Instrutor excluido com sucesso!');
 	    $scope.instrutores.splice(idInstrutor, 1);
     }
 
