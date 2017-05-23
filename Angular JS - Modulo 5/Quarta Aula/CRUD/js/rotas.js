e693afeb412ab865e60807af9939151cecb05aee
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-23 17:38:00 -0300

    Feat(Angular JS): Alterações legais

--- Angular JS - Modulo 5/Quarta Aula/CRUD/js/rotas.js
+++ Angular JS - Modulo 5/Quarta Aula/CRUD/js/rotas.js
@@ -1,19 +1,19 @@
     controller.config(function ($routeProvider) { 
         $routeProvider 
             .when('/adicionarAula', { 
-            controller: '/Aulas/Aula.Controller', 
+            controller: 'Aula.Controller', 
             templateUrl: './adicionarAula.html' 
         })
             .when('/exibirAulas', { 
-            controller: '/Aulas/Aula.Controller', 
+            controller: 'Aula.Controller', 
             templateUrl: './exibirAulas.html' 
         })
             .when('/adicionarInstrutor', { 
-            controller: '/Instrutor/Instrutor.Controller', 
+            controller: 'Instrutor.Controller', 
             templateUrl: 'adicionarInstrutor.html' 
         })
             .when('/exibirInstrutores', { 
-            controller: '/Instrutor/Instrutor.Controller', 
+            controller: 'Instrutor.Controller', 
             templateUrl: 'exibirInstrutores.html' 
         }) 
             .otherwise({redirectTo:'/'}); 
