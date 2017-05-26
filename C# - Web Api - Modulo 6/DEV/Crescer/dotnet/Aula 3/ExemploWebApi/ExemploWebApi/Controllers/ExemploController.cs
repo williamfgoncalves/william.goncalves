275122768e945a12293af9398e819dabe29ce56e
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-26 17:37:59 -0300

    Fea(C# Web Api): Aula 3

---
+++ C# - Web Api - Modulo 6/DEV/Crescer/dotnet/Aula 3/ExemploWebApi/ExemploWebApi/Controllers/ExemploController.cs
@@ -0,0 +1,44 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    // app start tem um arquivo com os comandos
+    // Exemplo,pois o controller no final é automático
+    public class ExemploController : ApiController
+    {
+        private static List<Heroi> herois = new List<Heroi>(); // lista de herois
+
+        public IEnumerable<Heroi> Get()
+        {
+            if(herois == null)
+            {
+                return herois;
+            }
+            else
+            {
+                return herois;
+            }
+        }
+
+        private static int id = 0;
+        public IHttpActionResult Post(Heroi heroi)
+        {
+            if(heroi == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                herois.Add(heroi);
+                heroi.Id = ++id;
+                return Ok(heroi);
+            }
+        }
+    }
+}
