604576e74884b1ac5db778e49d249531bc6516c5
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-31 02:11:47 -0300

    Feat(C# Web Api): Adicionar Post, Get e Delete nos Revisores

---
+++ C# - Web Api - Modulo 6/Aula 5/EditoraCrescer.Infraesturtura/EditoraCrescer.Infraesturtura/EditoraCrescer.Api/Controllers/RevisoresController.cs
@@ -0,0 +1,34 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using EditoraCrescer.Infraesturtura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    public class RevisoresController : ApiController
+    {
+        RevisoresRepositorio repositorio = new RevisoresRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            var Revisores = repositorio.Obter();
+            return Ok(Revisores);
+        }
+
+        public IHttpActionResult Post(Revisor revisor)
+        {
+            repositorio.Criar(revisor);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            repositorio.Remover(id);
+            return Ok();
+        }
+    }
+}
