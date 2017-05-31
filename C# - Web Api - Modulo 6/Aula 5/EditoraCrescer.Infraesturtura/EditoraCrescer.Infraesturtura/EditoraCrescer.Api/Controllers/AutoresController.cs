c9019b424f7861c96724c698644b8093766f82fc
William Gonçalves <william.goncalves@cwi.com.br>
2017-05-31 02:01:19 -0300

    Feat(C# Web Api): Adicionar Post e Delete Autores

---
+++ C# - Web Api - Modulo 6/Aula 5/EditoraCrescer.Infraesturtura/EditoraCrescer.Infraesturtura/EditoraCrescer.Api/Controllers/AutoresController.cs
@@ -0,0 +1,34 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using EditoraCrescer.Infraesturtura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    public class AutoresController : ApiController
+    {
+        AutoresRepositorio repositorio = new AutoresRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            var Autores = repositorio.Obter();
+            return Ok(Autores);
+        }
+
+        public IHttpActionResult Post(Autor autor)
+        {
+            repositorio.Criar(autor);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            repositorio.Remover(id);
+            return Ok();
+        }
+    }
+}
---
+++ C# - Web Api - Modulo 6/Aula 5/EditoraCrescer.Infraesturtura/EditoraCrescer.Infraesturtura/EditoraCrescer.Infraesturtura/Repositorios/AutoresRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EditoraCrescer.Infraesturtura.Entidades;
+
+namespace EditoraCrescer.Infraesturtura.Repositorios
+{
+    public class AutoresRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public List<Autor> Obter()
+        {
+            return contexto.Autores.ToList();
+        }
+
+        public void Criar(Autor autor)
+        {
+            contexto.Autores.Add(autor);
+            contexto.SaveChanges();
+        }
+
+        public void Remover(int id)
+        {
+            var autor = contexto.Autores.FirstOrDefault(x => x.Id == id);
+            contexto.Autores.Remove(autor);
+        }
+    }
+}
