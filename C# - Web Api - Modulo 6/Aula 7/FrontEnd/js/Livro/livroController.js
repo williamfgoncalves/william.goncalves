535157e7fae24efdaa4c536ecfe0cad0a0bb7bfb
William Gonçalves <william.goncalves@cwi.com.br>
2017-06-03 02:18:01 -0300

    Trabalho editora: Adicionar páginação

--- C# - Web Api - Modulo 6/Aula 7/FrontEnd/js/Livro/livroController.js
+++ C# - Web Api - Modulo 6/Aula 7/FrontEnd/js/Livro/livroController.js
@@ -5,6 +5,21 @@
       quantidadeTrazer: 8,
     };
 
+    $scope.retornarPagina = function(){
+            $scope.parametros.quantidadePular -= 8;
+            
+            if($scope.parametros.quantidadePular < 0){
+                $scope.parametros.quantidadePular = 0;
+                return;
+            }
+
+            listarlivrosNaoLancamento();
+    }
+    $scope.avancarPagina = function(){
+            $scope.parametros.quantidadePular += 8;
+            listarlivrosNaoLancamento();
+    }
+
     function listarTodosOsLivros(){
         livroService.listarTodosLivros().then(function (response){
             $scope.livrosTodos = response.data.dados;
@@ -26,6 +41,7 @@
     $scope.verLivro = function (id) {
         $location.path('/livros/' + id);
     }
+
     $scope.logar = function(){
         $location.path('/login');
     }
