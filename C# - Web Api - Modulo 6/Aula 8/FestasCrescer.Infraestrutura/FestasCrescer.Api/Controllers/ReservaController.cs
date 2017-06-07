ba5411c045647f28e0ff43737e081db5f5b01356
William Gonçalves <william.goncalves@cwi.com.br>
2017-06-07 18:09:12 -0300

    Feat(C# Web Api): Trabalho Final: Começando a parte de Reservas

---
+++ C# - Web Api - Modulo 6/Aula 8/FestasCrescer.Infraestrutura/FestasCrescer.Api/Controllers/ReservaController.cs
@@ -0,0 +1,22 @@
+﻿using FestasCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FestasCrescer.Api.Controllers
+{
+    public class ReservaController : ApiController
+    {
+        /Orcamento
+        public IHttpActionResult Post(ReservaModel model)
+        {
+            var festa = repositorio.ObterFEstaPorId(model.IdFesta);
+
+            var reserva = new Reserva(festa);
+            //repositorio.Salvar(reserva);
+        }
+    }
+}
